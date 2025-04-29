836743d3a9b7e6db138e9dc6df16cfd2a4bb85ca
last-blade <meprashanttyagi2000@gmail.com>
2025-04-29 23:31:05 +0530

    objects

---
+++ Step10-Objects/03_index.js
@@ -0,0 +1,9 @@
+const course = {
+    coursename: "Javascript",
+    courseInstructor: "Prashant",
+    price: "9999",
+};
+
+const {courseInstructor: instructor} = course;
+
+console.log(instructor);
