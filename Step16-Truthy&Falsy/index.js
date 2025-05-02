0db358888e73cc78cb9ea5c50ba6b44614689b34
last-blade <meprashanttyagi2000@gmail.com>
2025-05-02 17:58:08 +0530

    truthy and falsy values

---
+++ Step16-Truthy&Falsy/index.js
@@ -0,0 +1,5 @@
+//! Falsy values
+//^ false, 0, -0, NaN, null, undefined, "", bigint 0n
+
+//! Truthy values
+//^ oopar wali values ke alawa baaki sab truthy values hain, (" ", true, "0", "false", [], {}, function(){}, )
